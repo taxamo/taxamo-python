7c4144653d8fd11f35b0d3af1c17fe997ee1973f
Tomek Lipski <tomek.lipski@gmail.com>
2014-09-10 23:21:38 +0200

    updated version to 0.0.5

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ if sys.version_info < (3, 0):
 setup(
     name='taxamo',
     cmdclass={'build_py': build_py},
-    version='0.0.4',
+    version='0.0.5',
     description='Taxamo python bindings',
     author='Taxamo',
     author_email='support@taxamo.com',
